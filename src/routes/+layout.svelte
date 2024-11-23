300f0f05830f3de6ae6deddadf1a83ca740134e1
devH0K <devhok@gmail.com>
2024-11-22 22:24:40 -0300

    componente pagina

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-  import Logo from '../components/template/Logo.svelte';
+    import Pagina from '../components/template/Pagina.svelte';
 	let { children } = $props();
 </script>
-<div class="
-	flex flex-col justify-center items-center gap-5 bg-black text-white min-h-screen
-">
 
-<Logo/>
-
-{@render children()}
-</div>
+<Pagina>
+	{@render children()}
+</Pagina>
