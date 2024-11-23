0bb3e32d02401e30da2ef4f4d69f001b390cd9a1
devH0K <devhok@gmail.com>
2024-11-23 14:45:36 -0300

    menu item

--- src/components/template/AreaLateral.svelte
+++ src/components/template/AreaLateral.svelte
@@ -1,22 +1,26 @@
 <script>
   import Menu from "./Menu.svelte";
+    import MenuItem from "./MenuItem.svelte";
 </script>
 <aside class="flex flex-col gap-10 self-start border border-zinc-800 p-5 rounded-md w-72">
   <Menu titulo="Navegação">
-    <li>Teste #1</li>
-    <li>Teste #2</li>
-    <li>Teste #3</li>
+    <MenuItem texto="Início" link="/"/>
+    <MenuItem texto="Personagens" link="/"/>
+    <MenuItem texto="Autores" link="/autores"/>
+    <MenuItem texto="Séries" link="/"/>
   </Menu>
 
   <Menu titulo="Principais Autores">
-    <li>Teste #1</li>
-    <li>Teste #2</li>
-    <li>Teste #3</li>
+    <MenuItem texto="J.K Rowling" link="/"/>
+    <MenuItem texto="J.R.R. Tolkien" link="/"/>
+    <MenuItem texto="George R.R. Martin" link="/autores/george-martin"/>
   </Menu>
 
   <Menu titulo="Outras Categorias">
-    <li>Teste #1</li>
-    <li>Teste #2</li>
-    <li>Teste #3</li>
+    <MenuItem texto="Últimas Notícias" link="/"/>
+    <MenuItem texto="Séries Populares" link="/"/>
+    <MenuItem texto="Facebook" link="/"/>
+    <MenuItem texto="Instagram" link="/"/>
+    <MenuItem texto="Youtube" link="/"/>
   </Menu>
 </aside>
