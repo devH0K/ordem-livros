2a729304c5085bc0573a0981cd69b4ecca2f85ba
devH0K <devhok@gmail.com>
2024-11-23 00:56:07 -0300

    componente cabecalho

--- src/components/template/Pagina.svelte
+++ src/components/template/Pagina.svelte
@@ -1,10 +1,9 @@
 <script>
-    import Logo from "./Logo.svelte";
-
+  import Cabecalho from "./Cabecalho.svelte";
 </script>
 <div class="flex flex-col min-h-screen bg-black text-white">
 
-  <Logo/>
+  <Cabecalho/>
   <main>
     <slot></slot>
   </main>
