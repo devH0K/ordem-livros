d8aaa28df2d0e1423e6d054b83c1cf892f0e669c
devH0K <devhok@gmail.com>
2024-11-24 00:14:29 -0300

    conteudo serie

--- src/components/serie/ConteudoSerie.svelte
+++ src/components/serie/ConteudoSerie.svelte
@@ -2,7 +2,17 @@
   export let serie: any = null;
 </script>
 {#if serie}
-  <div>
-    {serie.id}
+  <div class="flex flex-col gap-5">
+    <h1 class="text-3xl font-black">{serie.titulo}</h1>
+    <a href="/autores/{serie.autor.id}" class="text-sm text-zinc-400 -mt-4">
+      {serie.autor.nome}
+    </a>
+    <img src={serie.imagem} alt="Imagem da Série" class="h-96 object-cover">
+    <p class="text-lg text-zinc-400 text-justify">{serie.descricao}</p>
+
+    <h2>Lista de Livros</h2>
+    {#each serie.livros as livro}
+      <div>{livro.titulo}</div>
+    {/each}
   </div>  
 {/if}
