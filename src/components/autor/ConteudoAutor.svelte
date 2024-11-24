7cd750f2c45f2e1b347b9abdf414b2d13c7e4556
devH0K <devhok@gmail.com>
2024-11-23 21:00:55 -0300

    conteudo autor parte 1

---
+++ src/components/autor/ConteudoAutor.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let autor:any = null;
+</script>
+{#if autor}
+<div class="flex flex-col gap-5">
+  <img src={autor.imagem} alt="Foto do Autor" class="h-96 object-cover rounded-md">
+  <article>
+    {autor.biografia}
+  </article>
+</div>
+{/if}
