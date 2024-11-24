b96f510493c11efccd54b8820f8e736a005620bb
devH0K <devhok@gmail.com>
2024-11-23 21:23:48 -0300

    autor item

---
+++ src/components/autor/ItemAutor.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let autor: any = null;
+</script>
+
+{#if autor}
+  <a href="/autores/{autor.id}"
+    class="flex flex-col rounded-md relative overflow-hidden border border-zinc-800"
+  >
+    <img src={autor.imagem} alt="Foto do Autor" class="h-96 object-cover">
+    <div class="absolute bottom-0 flex px-6 py-4 bg-black/50 w-full justify-end ">
+      <span class="text-2xl font-black">{autor.nome}</span>
+    </div>
+  </a>
+{/if}
---
+++ src/routes/autores/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import ItemAutor from "../../components/autor/ItemAutor.svelte";
+  import autores from "../../constants/autores";
+</script>
+<a href="/autores">Autores</a>
+
+<div class="grid grid-cols-2 gap-5">
+  {#each autores as autor}
+    <ItemAutor autor={autor}/>
+  {/each}
+</div>
